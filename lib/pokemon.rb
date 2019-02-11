2eb1292bca664b41b9d6936d5c297b6965886a9b
Monica Marziano <mmarziano1@gmail.com>
2019-02-11 10:48:27 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,6 +1,11 @@
 class Pokemon
-
-  
- 
+    attr_accessor :name, :id, :type, :db 
+    
+    def initialize(name:, id:, type: db:)
+      @name = name
+      @id = id
+      @type = type
+      @db = db
+    end 
   
 end
