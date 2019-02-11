aac956f9bdc8a49edbbad1e0652b0047053036d1
Monica Marziano <mmarziano1@gmail.com>
2019-02-11 11:01:27 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -16,7 +16,7 @@ class Pokemon
     end 
   
     def self.find(id, db)  
-      db.execute("SELECT * FROM pokemon WHERE id = ?", id)
+      new_pokemon = db.execute("SELECT * FROM pokemon WHERE id = ?", id)
       
     end 
   
